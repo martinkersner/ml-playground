3ec517945f9a981e9e63cf358e979a3bf555e23e
Martin Keršner <m.kersner@gmail.com>
2016-08-08 16:31:24 +0900

    modified data generation; added plotting data

--- GMM/EM.py
+++ GMM/EM.py
@@ -3,25 +3,47 @@
 # 2016/08/08
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
 def main():
-    print generate_data()
+    X = generate_data()
+    plot_data(X)
 
 def generate_data():
-    shifted_data = generate_shifted_gaussian(shift=[20, 20])
-    stretched_data = generate_stretched_gaussian()
-    data = np.vstack([shifted_data, stretched_data])
+    data = np.vstack([  shift(generate_gaussian(), offset=[10, 10]), \
+                        stretch(generate_gaussian()), \
+                        shift(stretch(generate_gaussian(), C=[[1.5, 0.4],[1.9, 1.1]]), offset=[-10, 10]) \
+                    ])
 
     return data
 
-def generate_shifted_gaussian(n_dims=2, n_samples=300, shift=[0, 0]):
-    return np.random.randn(n_samples, n_dims) + np.array(shift)
+def shift(data, offset=[0, 0]):
+    return data + np.array(offset)
 
-def generate_stretched_gaussian(n_samples=300):
-    C = np.array([[0., -0.7], [3.5, .7]])
-    return np.dot(np.random.randn(n_samples, 2), C)
+def stretch(data, C=[[0.0, 0.7], [8.5, 1.0]]):
+    return np.dot(data, np.array(C))
+
+def generate_gaussian(n_samples=300, n_dims=2):
+    return np.random.randn(n_samples, n_dims)
+
+def plot_data(data, margin=10):
+    min_x = np.min(data[:, 0])
+    min_y = np.min(data[:, 1])
+    max_x = np.max(data[:, 0])
+    max_y = np.max(data[:, 1])
+
+    x = np.linspace(min_x+margin, max_x+margin)
+    y = np.linspace(min_y+margin, max_y+margin)
+
+    X, Y = np.meshgrid(x, y)
+    XX = np.array([X.ravel(), Y.ravel()]).T
+
+    plt.scatter(data[:, 0], data[:, 1], 0.5)
+
+    plt.axis('tight')
+    plt.show()
 
 if __name__ == "__main__":
     main()
